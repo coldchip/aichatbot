183f1b177218959bc177bf0708b859c2a9861325
coldchip <growwick@gmail.com>
2017-10-18 00:16:17 +0800

    Update rsa.php

--- rsa.php
+++ rsa.php
@@ -61,27 +61,31 @@ class RSA extends ColdChipMath {
 		$result = '';
 		do {
 			$result .= chr(bcmod($num, '256'));
-			$num = bcdiv($num, '256');
+			$num = gmp_div($num, '256');
 		} while (bccomp($num, '0'));
 		return $result;
 	}
 	
-	function Encrypt($msg, $data)
+	function Encrypt($msg)
 	{
-		$this->data = base64_decode($data);
+		$this->data = base64_decode($this->publicKey);
 		$this->exp = base64_decode(json_decode($this->data, true)["exp"]);
 		$this->mod = base64_decode(json_decode($this->data, true)["mod"]);
 		$this->encryptData = array();
 		$this->encryptData["msg"] = $msg;
 		$this->encryptData["nounce"] = hash("crc32", rand(100000, 999999));
 		$this->encodedMsg = $this->textDec(json_encode($this->encryptData));
-		$this->encryptedData = $this->PowerMod($this->encodedMsg, $this->exp, $this->mod);
-		return base64_encode($this->encryptedData);
+		if(strlen($this->encodedMsg) <= strlen($this->mod)) {
+			$this->encryptedData = $this->PowerMod($this->encodedMsg, $this->exp, $this->mod);
+			return base64_encode($this->encryptedData);
+		} else {
+			return false;
+		}
+		
 	}
 	
-	function Decrypt($encryptedMsg, $data)
-	{
-		$this->data = base64_decode($data);
+	function Decrypt($encryptedMsg) {
+		$this->data = base64_decode($this->privateKey);
 		$this->exp = base64_decode(json_decode($this->data, true)["exp"]);
 		$this->mod = base64_decode(json_decode($this->data, true)["mod"]);
 		$this->decryptedMsg = $this->PowerMod(base64_decode($encryptedMsg), $this->exp, $this->mod);
@@ -152,31 +156,181 @@ class RSA extends ColdChipMath {
 
 $rsa = new RSA();
 
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+	if($_POST["mode"] == "encrypt")
+	{
+		$rsa->publicKey = $_POST["pubkey"];
+		$encryptedData = $rsa->Encrypt($_POST["msg"]);
+		if($encryptedData) {
+			die('{"data":"' . $encryptedData . '"}');
+		} else {
+			die('{"data":"Message is too long. "}');
+		}
+	}
+	
+	if($_POST["mode"] == "decrypt")
+	{
+		$rsa->privateKey = $_POST["prikey"];
+		die('{"data":"' . $rsa->Decrypt($_POST["msg"]) . '"}');
+	}
+	
+	if($_POST["mode"] == "genkeys")
+	{
+		$rsa->generateKeys(1024);
+		$packer = array();
+		$packer["pubkey"] = $rsa->publicKey;
+		$packer["prikey"] = $rsa->privateKey;
+		die(json_encode($packer));
+	}
+}
 
-$rsa->generateKeys(128);
 
-echo('<textarea style="width: 500px; height: 100px;">' . $rsa->publicKey . "</textarea><br>");
-echo('<textarea style="width: 500px; height: 100px;">' . $rsa->privateKey . "</textarea><br>");
-
-
-// BIT: 7250
-
-/*
-
-$encryptKey = "";
-$decryptKey = "";
-
-$msg = "RSA (Rivest–Shamir–Adleman) is one of the first practical public-key cryptosystems and is widely used for secure data transmission. In such a cryptosystem, the encryption key is public and it is different from the decryption key which is kept secret (private). ";
-
-echo("Message: " . $msg . "<br>");
-
-$enc = $rsa->Encrypt($msg, $encryptKey);
-
-
-echo "encrypted_message: " . $enc . "<br>";
-
-echo("decrypted_message: " . $rsa->Decrypt($enc, $decryptKey));
-
-*/
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>ColdChip - Translate</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />		
+<link rel="shortcut icon" type="image/png" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="/>
+<meta charset="utf-8">	
+<script>
+function decrypt()
+{
+	var prikey = document.getElementById("prikey").value;
+	var msg = document.getElementById("msg").value;
+	if(!prikey)
+	{
+		alert("Private Key is empty. ");
+		msg.value = "";
+		return false;
+	}
+	var http = new XMLHttpRequest();
+	http.open("POST", window.location.pathname + window.location.search, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.send("msg=" + encodeURI(msg) + "&prikey=" + encodeURI(prikey) + "&mode=decrypt");
+	http.onreadystatechange = function()
+	{
+		if(this.readyState == 4 && this.status == 200)
+		{
+			var result = JSON.parse(http.responseText);
+			document.getElementById("msg").value = result["data"];
+		}
+	};
+}
+
+function encrypt()
+{
+	var pubkey = document.getElementById("pubkey").value;
+	var msg = document.getElementById("msg").value;
+	if(!pubkey)
+	{
+		alert("Public Key is empty. ");
+		msg.value = "";
+		return false;
+	}
+	var http = new XMLHttpRequest();
+	http.open("POST", window.location.pathname + window.location.search, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.send("msg=" + encodeURI(msg) + "&pubkey=" + encodeURI(pubkey) + "&mode=encrypt");
+	http.onreadystatechange = function()
+	{
+		if(this.readyState == 4 && this.status == 200)
+		{
+			var result = JSON.parse(http.responseText);
+			document.getElementById("msg").value = result["data"];
+		}
+	};
+}
+
+function genkeys()
+{
+	var http = new XMLHttpRequest();
+	http.open("POST", window.location.pathname + window.location.search, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.send("mode=genkeys");
+	http.onreadystatechange = function()
+	{
+		if(this.readyState == 4 && this.status == 200)
+		{
+			var result = JSON.parse(http.responseText);
+			document.getElementById("pubkey").value = result["pubkey"];
+			document.getElementById("prikey").value = result["prikey"];
+		}
+	};
+}
+</script>
+<style>
+*{
+	-webkit-appearance: none;
+	-moz-appearance: none;
+	-ms-appearance: none;
+	-o-appearance: none;
+	direction: ltr;
+	outline:0;
+	font-family: Arial, sans-serif;
+}body{
+	background: #f0f0f0;
+	margin:0;
+	padding:0;
+}.text{
+	color: #505050;
+	text-align: center;
+	margin-top: 0px;
+	margin-bottom: 0px;
+}.textarea{
+	box-sizing:border-box;
+	text-align: left;
+	width:100%;
+	height: 80px;
+	font-size: 18px;
+	font-weight: normal;
+	color: #3f3f3f;
+	border: 1px solid #bbb;
+	padding-left: 5px;
+	border-radius: 3px;
+	margin: 1px 0px 15px 0px;
+	box-shadow: inset 0px 1px 2px rgba(27,31,35,0.075);
+}.button{
+	font-weight: bold;
+	width: 100%;
+	height: 38px;
+	margin: 5px 0px;
+	color: #fff;
+	border: none;
+	background-color: #269f42;
+	font-size:15px;
+	border-radius: 3px;
+	box-sizing:border-box;
+}.header{
+	display:inline-block;
+	visibility:visible;
+	width:100%;
+	height:50px;
+	background: #fff;
+	margin-bottom:10px;
+	box-shadow:0 5px 10px #aaa;
+	position: fixed;
+}.content{
+	padding-top: 80px;
+	width: 95%;
+	margin: auto;
+}
+</style>
+</head>
+<body>
+<div class="header">
+<p class="text" style="float: left; margin-left: 20px; font-size: 20px; font-weight: 300; margin-top: 12px;">ColdChip Translate</p>
+</div>
+<div class="content">
+<textarea class="textarea" placeholder="Public Key. " id="pubkey"></textarea>
+<textarea class="textarea" placeholder="Private Key. " id="prikey"></textarea>
+<textarea class="textarea" placeholder="Message. " id="msg"></textarea>
+<button class="button" onClick="encrypt()">Encrypt</button>
+<button class="button" onClick="decrypt()">Decrypt</button>
+<button class="button" onClick="genkeys()">Generate Keys</button>
+
+</div>
+</body>
+</html>
